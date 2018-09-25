9a9ced70014965551906ea8a69e9a308ee23e5ec
GmGniap <thetpaimyo103@gmail.com>
2018-09-25 14:18:20 +0630

    ROS Test Pack

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/gmgniap/robot/src
+# Build directory: /home/gmgniap/robot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(walker)
---
+++ build/walker/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gmgniap/robot/src/walker
+# Build directory: /home/gmgniap/robot/build/walker
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
